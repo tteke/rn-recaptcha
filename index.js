ed69f0664cc4bd2c40aa94d8f69f4ad99c4c81aa
Turker Teke <trkrtk@gmail.com>
2019-09-11 12:36:57 +0300

    changed to recaptcha v3

--- index.js
+++ index.js
@@ -15,21 +15,37 @@ const patchPostMessageJsCode = `(${String(function() {
 })})();`;
 
 const generateTheWebViewContent = siteKey => {
+  // v3 recaptcha
   const originalForm =
-    '<!DOCTYPE html><html><head>' +
-    '<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge">' +
-    '<script src="https://recaptcha.google.com/recaptcha/api.js"></script>' +
-    '<script type="text/javascript"> var onloadCallback = function() { }; ' +
-    'var onDataCallback = function(response) { console.log(response); window.postMessage(response);  }; ' +
-    'var onDataExpiredCallback = function(error) {  window.postMessage("expired"); }; ' +
-    'var onDataErrorCallback = function(error) {  window.postMessage("error"); } </script>' +
-    '</head><body>' +
-    '<div style="text-align: center"><div class="g-recaptcha" style="display: inline-block"' +
-    'data-sitekey="' +
-    siteKey +
-    '" data-callback="onDataCallback" ' +
-    'data-expired-callback="onDataExpiredCallback" ' +
-    'data-error-callback="onDataErrorCallback"></div></div></body></html>';
+    '<!DOCTYPE html><html><head> ' +
+    '<style>  .text-xs-center { text-align: center; } .g-recaptcha { display: inline-block; } </style> ' +
+    '<script src="https://www.google.com/recaptcha/api.js?render=' + siteKey + '"></script> ' +
+    '<script type="text/javascript"> ' +
+    'grecaptcha.ready(function() { ' +
+        `(${String(onReady)})(); ` +
+        'grecaptcha.execute(\'' + siteKey + '\', {action: \'' + action + '\'}).then( '+
+            'function (responseToken) { window.postMessage(responseToken);  } ' +
+        ' ); ' +
+    '}); ' +
+    '</script>' +
+    '</head></html>'
+
+  // v2 recaptcha
+  // const originalForm =
+  //   '<!DOCTYPE html><html><head>' +
+  //   '<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge">' +
+  //   '<script src="https://recaptcha.google.com/recaptcha/api.js"></script>' +
+  //   '<script type="text/javascript"> var onloadCallback = function() { }; ' +
+  //   'var onDataCallback = function(response) { console.log(response); window.postMessage(response);  }; ' +
+  //   'var onDataExpiredCallback = function(error) {  window.postMessage("expired"); }; ' +
+  //   'var onDataErrorCallback = function(error) {  window.postMessage("error"); } </script>' +
+  //   '</head><body>' +
+  //   '<div style="text-align: center"><div class="g-recaptcha" style="display: inline-block"' +
+  //   'data-sitekey="' +
+  //   siteKey +
+  //   '" data-callback="onDataCallback" ' +
+  //   'data-expired-callback="onDataExpiredCallback" ' +
+  //   'data-error-callback="onDataErrorCallback"></div></div></body></html>';
   return originalForm;
 };
 
